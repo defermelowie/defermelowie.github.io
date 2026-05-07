a36c5103555ba0cad3fa55c88099405842f8693d
Lowie Deferme <lowie.deferme@kuleuven.be>
2026-05-07 10:06:24 +0200

    feat(xtask): add subcommand for building wasm modules

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    env, fs,
+    env, ffi, fs,
     path::{Path, PathBuf},
 };
 
@@ -15,6 +15,26 @@ fn source_dir() -> Result<PathBuf> {
     Path::new("site/")
         .canonicalize()
         .context("site source directory does not exist")
+}
+
+/// Get a list of source directories for all wasm modules
+fn wasm_source_dirs() -> Result<Vec<PathBuf>> {
+    let entries = Path::new("wasm/")
+        .canonicalize()
+        .map(|p| p.read_dir())
+        .flatten()
+        .context("could not read 'wasm' dir")?;
+    let subdirs = entries
+        .filter_map(|res| res.ok())
+        .map(|entry| entry.path())
+        .filter(|path| path.is_dir())
+        .collect();
+    Ok(subdirs)
+}
+
+/// Get path of site's static directory
+fn static_dir() -> Result<PathBuf> {
+    source_dir().map(|path| path.join("static"))
 }
 
 fn build_dir() -> Result<PathBuf> {
@@ -42,6 +62,8 @@ enum Commands {
     Build,
     /// Serve the site locally for development
     Serve,
+    /// Build and update wasm modules
+    Wasm,
     /// Clean build artifacts
     Clean,
 }
@@ -54,6 +76,7 @@ fn main() -> Result<()> {
         Commands::Setup => setup(&sh),
         Commands::Build => build(&sh),
         Commands::Serve => serve(&sh),
+        Commands::Wasm => wasm(&sh),
         Commands::Clean => clean(&sh),
     }
 }
@@ -67,7 +90,14 @@ fn setup(sh: &Shell) -> Result<()> {
     cmd!(sh, "git submodule update --init").run()?;
 
     // Install zola
-    cmd !(sh, "cargo install --locked --git https://github.com/getzola/zola").run()?;
+    cmd!(
+        sh,
+        "cargo install --locked --git https://github.com/getzola/zola"
+    )
+    .run()?;
+
+    // Install wasm-pack
+    cmd!(sh, "cargo install --locked wasm-pack").run()?;
 
     // Return success
     Ok(())
@@ -96,6 +126,42 @@ fn build(sh: &Shell) -> Result<()> {
     cmd!(sh, "zola build --output-dir {output_dir}").run()?;
 
     // Return success
+    Ok(())
+}
+
+fn wasm(sh: &Shell) -> Result<()> {
+    let static_dir = static_dir()?;
+    for dir in wasm_source_dirs()? {
+        // Build wasm
+        sh.change_dir(&dir);
+        cmd!(sh, "wasm-pack build --target web --out-dir pkg").run()?;
+        // Determine which output files to copy
+        let output_dir = dir.join("pkg").canonicalize()?;
+        let copy_files = output_dir
+            .read_dir()?
+            .filter_map(|r| r.ok())
+            .map(|e| e.path())
+            .filter(|p| p.is_file())
+            .filter(|p| {
+                let ext = p.extension().and_then(|s| s.to_str());
+                match ext {
+                    Some("js") => true,
+                    Some("wasm") => true,
+                    Some(_) => false,
+                    None => false,
+                }
+            })
+            .collect::<Vec<PathBuf>>();
+        // Copy these output files to the blog's static folder
+        for file in copy_files {
+            let dest_path = static_dir.join(file.file_name().unwrap());
+            fs::copy(&file, &dest_path).context(format!(
+                "Failed to copy {} to {}",
+                file.display(),
+                dest_path.display()
+            ))?;
+        }
+    }
     Ok(())
 }
 
